ac688755d9df21179cfc88febb4a835b896dbc42
Angel Zelada <azeladad@miumg.edu.gt>
2026-08-26 20:20:22 -0600

    Estructura inicial del proyecto Spring Boot con Maven

---
+++ src/main/java/com/lab/spring_apis_lab/SpringApisLabApplication.java
@@ -0,0 +1,13 @@
+package com.lab.spring_apis_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApisLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApisLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/spring_apis_lab/SpringApisLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.spring_apis_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApisLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
